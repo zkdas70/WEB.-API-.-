dad4a95a21dfb52928eddd7ffa1974edf80ef68d
kostya zinich <kostya@zinich.su>
2022-02-10 20:12:18 +0700

    первая задача есть QT дизигнер

--- Большая задача по Maps API/Большая задача по Maps API. Часть №1/Большая задача по Maps API. Часть №1.py
+++ Большая задача по Maps API/Большая задача по Maps API. Часть №1/Большая задача по Maps API. Часть №1.py
@@ -4,31 +4,33 @@ import sys
 import requests
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5 import uic
 
 SCREEN_SIZE = [600, 450]
 
 
 class Example(QWidget):
     static_uri = 'http://static-maps.yandex.ru/1.x/'
+
     def __init__(self):
         super().__init__()
+        uic.loadUi('ui.ui', self)
         self.coords = [37.530887, 55.7031181]
         self.scale = 0.002
         self.map_type = 'map'
         self.getImage()
-        self.initUI()
 
 
 
     def getImage(self):
         map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"
-        #response = requests.get(map_request)
+        # response = requests.get(map_request)
         params = {
             'll': ','.join(map(str, self.coords)),
             'spn': f'{self.scale},{self.scale}',
             'l': self.map_type
         }
-        response = requests.get(self.static_uri, params = params)
+        response = requests.get(self.static_uri, params=params)
 
         if not response:
             print("Ошибка выполнения запроса:")
@@ -37,22 +39,9 @@ class Example(QWidget):
             sys.exit(1)
 
         self.map = response.content
-
-    def initUI(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
-        self.setWindowTitle('Отображение карты')
-
-        ## Изображение
         self.pixmap = QPixmap()
         self.pixmap.loadFromData(self.map, 'PNG')
-        self.image = QLabel(self)
-        self.image.move(0, 0)
-        self.image.resize(600, 450)
         self.image.setPixmap(self.pixmap)
-
-    def closeEvent(self, event):
-        """При закрытии формы подчищаем за собой"""
-        os.remove(self.map_file)
 
 
 if __name__ == '__main__':
