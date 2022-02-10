5ba6ac84dac21f1e6ed465823867256c3a7ddb63
kostya zinich <kostya@zinich.su>
2022-02-10 20:16:07 +0700

    первая задача есть QT дизигнер периминоан файл ФИНАЛ

--- Большая задача по Maps API/Большая задача по Maps API. Часть №1/Большая задача по Maps AP.py
+++ Большая задача по Maps API/Большая задача по Maps API. Часть №1/Большая задача по Maps AP.py
@@ -20,8 +20,6 @@ class Example(QWidget):
         self.map_type = 'map'
         self.getImage()
 
-
-
     def getImage(self):
         map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"
         # response = requests.get(map_request)
