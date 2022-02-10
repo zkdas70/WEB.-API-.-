64c25bced782c3ac4988fd772e9cfc1bbe2f945b
kostya zinich <kostya@zinich.su>
2022-02-10 19:57:39 +0700

    первая задача 2

--- Большая задача по Maps API/Большая задача по Maps API. Часть №1/Большая задача по Maps API. Часть №1.py
+++ Большая задача по Maps API/Большая задача по Maps API. Часть №1/Большая задача по Maps API. Часть №1.py
@@ -9,14 +9,26 @@ SCREEN_SIZE = [600, 450]
 
 
 class Example(QWidget):
+    static_uri = 'http://static-maps.yandex.ru/1.x/'
     def __init__(self):
         super().__init__()
+        self.coords = [37.530887, 55.7031181]
+        self.scale = 0.002
+        self.map_type = 'map'
         self.getImage()
         self.initUI()
 
+
+
     def getImage(self):
         map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"
-        response = requests.get(map_request)
+        #response = requests.get(map_request)
+        params = {
+            'll': ','.join(map(str, self.coords)),
+            'spn': f'{self.scale},{self.scale}',
+            'l': self.map_type
+        }
+        response = requests.get(self.static_uri, params = params)
 
         if not response:
             print("Ошибка выполнения запроса:")
@@ -24,17 +36,15 @@ class Example(QWidget):
             print("Http статус:", response.status_code, "(", response.reason, ")")
             sys.exit(1)
 
-        # Запишем полученное изображение в файл.
-        self.map_file = "map.png"
-        with open(self.map_file, "wb") as file:
-            file.write(response.content)
+        self.map = response.content
 
     def initUI(self):
         self.setGeometry(100, 100, *SCREEN_SIZE)
         self.setWindowTitle('Отображение карты')
 
         ## Изображение
-        self.pixmap = QPixmap(self.map_file)
+        self.pixmap = QPixmap()
+        self.pixmap.loadFromData(self.map, 'PNG')
         self.image = QLabel(self)
         self.image.move(0, 0)
         self.image.resize(600, 450)
